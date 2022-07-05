e4233e04ee2e000b9471fdeec28a61584bba281d
neonowy <michal.burzynski@kyndryl.com>
2022-07-05 11:24:11 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/michalb-training/s3-bucket/aws"
   version = "2.8.0"
   
-  bucket = "my-s3-bucket"
+  bucket = "my-s3-bucket1"
   acl    = "private"
 
   versioning = {
